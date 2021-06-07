4dbbcee20a54f37ce7acaec92f86272784a40d17
dangcq-1903 <cao.quy.dang@sun-asterisk.com>
2021-06-07 14:37:47 +0700

    feat(api): test sun ci running

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->describe('Show an inspiring quote');
