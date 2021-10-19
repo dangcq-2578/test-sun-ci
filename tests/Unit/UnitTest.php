6f57856867b9a05b9b333fe791e5baa4100f15e9
dangcq-1903 <cao.quy.dang@sun-asterisk.com>
2021-10-19 14:29:03 +0700

    add test

---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+
+    public function testBasic()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+
+    public function testNoBasic()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+
+    public function testSuperBasic()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+
+    public function testSuperBasic1()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+
+    public function testSuperBasic2()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+
+    public function testSuperBasic3()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+
+    public function testSuperBasic4()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+
+    public function testSuperBasic5()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals([], []);
+    }
+}
